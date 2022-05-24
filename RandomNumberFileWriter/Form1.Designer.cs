0732795a353b7a4401d84ec2e96edad5006afd76
KassandraSaint <kassandrastripper@gmail.com>
2022-05-24 12:33:10 -0400

    Add project files.

---
+++ RandomNumberFileWriter/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿
+namespace RandomNumberFileWriter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.numberBox = new System.Windows.Forms.TextBox();
+            this.genBtn = new System.Windows.Forms.Button();
+            this.saveFile = new System.Windows.Forms.SaveFileDialog();
+            this.closeBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(171, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(455, 72);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Specify How Many Random \r\nNumbers You Want to Generate";
+            // 
+            // numberBox
+            // 
+            this.numberBox.Font = new System.Drawing.Font("Microsoft YaHei", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numberBox.Location = new System.Drawing.Point(269, 116);
+            this.numberBox.Name = "numberBox";
+            this.numberBox.Size = new System.Drawing.Size(280, 43);
+            this.numberBox.TabIndex = 1;
+            // 
+            // genBtn
+            // 
+            this.genBtn.Font = new System.Drawing.Font("Microsoft YaHei", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.genBtn.Location = new System.Drawing.Point(177, 184);
+            this.genBtn.Name = "genBtn";
+            this.genBtn.Size = new System.Drawing.Size(200, 80);
+            this.genBtn.TabIndex = 2;
+            this.genBtn.Text = "Generate";
+            this.genBtn.UseVisualStyleBackColor = true;
+            this.genBtn.Click += new System.EventHandler(this.genBtn_Click);
+            // 
+            // closeBtn
+            // 
+            this.closeBtn.Font = new System.Drawing.Font("Microsoft YaHei", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.closeBtn.Location = new System.Drawing.Point(426, 184);
+            this.closeBtn.Name = "closeBtn";
+            this.closeBtn.Size = new System.Drawing.Size(200, 80);
+            this.closeBtn.TabIndex = 3;
+            this.closeBtn.Text = "Exit";
+            this.closeBtn.UseVisualStyleBackColor = true;
+            this.closeBtn.Click += new System.EventHandler(this.closeBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 310);
+            this.Controls.Add(this.closeBtn);
+            this.Controls.Add(this.genBtn);
+            this.Controls.Add(this.numberBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Random Number File Writer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox numberBox;
+        private System.Windows.Forms.Button genBtn;
+        private System.Windows.Forms.SaveFileDialog saveFile;
+        private System.Windows.Forms.Button closeBtn;
+    }
+}
+
---
+++ RandomNumberFileWriter/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace RandomNumberFileWriter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void genBtn_Click(object sender, EventArgs e)
+        {
+            // Initializing variables to hold information for the loop
+            int nOfNumbers;
+            int randomNum;
+
+            //Creating a Random object
+            Random random = new Random();
+
+            // Validating a correct input from the user
+            if (int.TryParse(numberBox.Text, out nOfNumbers))
+            {
+                // Initializing a StreamReader variable
+                StreamWriter outputFile;
+
+                // Saving the file on a Desktop or showing an error message
+                if (saveFile.ShowDialog() == DialogResult.OK)
+                {
+                    // Creating a file with the specified name
+                    outputFile = File.CreateText(saveFile.FileName);
+
+                    // Generating random numbers and writing them to a specified file
+                    for (int counter = 0; counter < nOfNumbers; counter++)
+                    {
+                        // Generating a random number
+                        randomNum = random.Next(1, 101);
+                        // Writing a number to a file
+                        outputFile.WriteLine(randomNum);
+                    }
+                    // Closing the file
+                    outputFile.Close();
+                }
+                else
+                {
+                    // Showing a message if user clicked Cancel button
+                    MessageBox.Show("Save as was Canceled");
+                }
+            }
+            else
+            {
+                // Showing a message if input was wrong
+                MessageBox.Show("Wrong input. Enter a valid intager number.");
+            }
+        }
+
+        private void closeBtn_Click(object sender, EventArgs e)
+        {
+            // Closing the form
+            this.Close();
+        }
+    }
+}
